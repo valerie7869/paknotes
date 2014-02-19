18ec366e068809bef5dc8534cae46a3878cb701e
valerie <valerie.douglas@openlogic.com>
2014-02-18 21:14:54 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paknotes::Application.config.secret_token = '4ff7c99a92caf8c13db005505643d8958f96454d2f449466a1baf03cfbb77fa1a6f13df0b8f009d0afc066e0e0f1b65ba7226627807caae9a7ee25ca58d5183d'
